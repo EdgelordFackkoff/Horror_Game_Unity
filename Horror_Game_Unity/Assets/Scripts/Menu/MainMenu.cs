2b61bfafe9fde6012a2dcd262a51a4235efa2da8
P4ndemonium <54048657+P4ndemonium@users.noreply.github.com>
2024-08-13 00:56:12 +0800

    Added Main Menu and scene transitioning
    alot of files cause imported textmeshpro
    if this broke anything just revert to last commit

---
+++ Horror_Game_Unity/Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadLevelSelection()
+    {
+        SceneManager.LoadScene("Level_Selection_Menu");
+    }
+
+    public void LoadSettings()
+    {
+        SceneManager.LoadScene("Settings_Menu");
+    }
+
+    public void QuitGame()
+    {
+        // Only works in build not editor
+        Application.Quit();
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("Main_Menu");
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // REMEMBER TO UPDATE TO SCENE NAMES
+    // ALSO REMEMBER TO ADD YOUR SCENE TO THE BUILD IN:
+    // File -> Build Settings -> Add Open Scenes(click with your scene open)
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Testing_Grounds"); // Level_1
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level_2");
+    }
+
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene("Level_3");
+    }
+    ///////////////////////////////////////////////////////////////////////////
+}
