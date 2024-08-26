eca683618715007fa3dc70ecb18305fc295a1baa
EdgelordFackkoff <edgelordfackoff@gmail.com>
2024-08-26 19:56:03 +0800

    Push

--- Horror_Game_Unity/Assets/Scripts/Enemies/Librarian/Librarian.cs
+++ Horror_Game_Unity/Assets/Scripts/Enemies/Librarian/Librarian.cs
@@ -1,21 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Diagnostics;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Librarian : Enemy
 {
     [Header("Unique")]
+    [SerializeField] private GameObject detection_box;
     [SerializeField] public float roam_interval;
     [SerializeField] protected AudioSource smack_source;
     [SerializeField] private bool is_moving;
     [SerializeField] private int current_state;
     [SerializeField] private float timer;
+    [SerializeField] private float sightRange = 20f;
+    [SerializeField] private float detection_range = 10f;
     [SerializeField] private float speed_0 = 1.7f;
     [SerializeField] private float speed_1 = 2.0f;
     [SerializeField] private float speed_2 = 2.2f;
     [SerializeField] private float speed_3 = 2.4f;
+    [Header("Waypoints")]
+    [SerializeField] public Transform[] waypoints;
+    [SerializeField] public Transform current_waypoint;
+    [SerializeField] private int current_index = -1;
+    [SerializeField] private Vector3 last_known_location;
+    [SerializeField] public LayerMask playerLayer = 3;
+    [SerializeField] public LayerMask envLayer = 11;
+    [SerializeField] public bool searching = false;
+    [Header("Lost Player")]
+    [SerializeField] private float search_timer;
+    [SerializeField] private float search_duration = 5.0f;
+    [SerializeField] private float wander_radius = 3.0f;
 
     public override void Start()
     {
@@ -65,8 +83,8 @@ public class Librarian : Enemy
             current_state = 2;
         }
 
-        //Walking
-        if (current_state == 2)
+        //Patrol/Moving
+        if (current_state == 2 || current_state == 3)
         {
             //Walk
             if (anim_info.IsName("Walk"))
@@ -82,7 +100,7 @@ public class Librarian : Enemy
         }
 
         //If attacking
-        if (current_state == 3)
+        if (current_state == 4)
         {
             if (anim_info.IsName("Attack"))
             {
@@ -109,8 +127,8 @@ public class Librarian : Enemy
                     animator.SetBool("Idle", true);
                     animator.SetBool("Attacking", false);
                     animator.SetBool("Walk", false);
-                    //Set back to idle state
-                    current_state = 0;
+                    //Set back to hunting state
+                    current_state = 3;
                     isattacking = false;
                     can_attack = true;
                 }
@@ -134,14 +152,75 @@ public class Librarian : Enemy
     {
 
         //If hunting
-        if (current_state == 1 || current_state == 2)
+        if (current_state == 2)
+        {
+            if (searching == false)
+            {
+                //Now waypoints
+                if (current_waypoint != null)
+                {
+                    // Move towards the target waypoint
+                    agent.enabled = true;
+                    agent.SetDestination(current_waypoint.position);
+                    //Turn in current moving direction
+                    // Calculate the direction to the target waypoint
+                    Vector3 directionToWaypoint = (current_waypoint.position - transform.position).normalized;
+                    // Rotate towards the target waypoint
+                    if (directionToWaypoint != Vector3.zero)
+                    {
+                        Quaternion targetRotation = Quaternion.LookRotation(directionToWaypoint);
+                        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 1.00f * Time.deltaTime);
+                    }
+
+                    // Check if the librarian has reached the waypoint
+                    if (Vector3.Distance(transform.position, current_waypoint.position) < 1.10f)
+                    {
+                        SetNextWaypoint();
+                    }
+                }
+                else
+                {
+                    SetNextWaypoint();
+                }
+            }
+            else
+            {
+                //IF searching
+                if (!agent.pathPending && agent.remainingDistance < 0.2f)
+                {
+                    if (search_timer > 0)
+                    {
+                        search_timer -= Time.deltaTime;
+
+                        // Wander around the last known location
+                        Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * wander_radius;
+                        randomDirection += last_known_location;
+                        NavMeshHit hit;
+                        if (NavMesh.SamplePosition(randomDirection, out hit, wander_radius, 1))
+                        {
+                            Vector3 finalPosition = hit.position;
+                            agent.SetDestination(finalPosition);
+                        }
+                    }
+                    else
+                    {
+                        searching = false;
+                        SetNextWaypoint();
+                    }
+                }
+            }
+
+        }
+
+        //Knows where the palyer is
+        if (current_state == 3)
         {
             //Update Player location
             UpdatePlayer();
         }
 
         //If attacking
-        if (current_state == 3)
+        if (current_state == 4)
         {
             agent.enabled = false;
         }
@@ -150,18 +229,32 @@ public class Librarian : Enemy
     public override void Handle_Behaviour()
     {
 
+        switch (current_state)
+        {
+            case 2:
+                DetectPlayer();
+                break;
+            case 3:
+                DetectHidingPlayer();
+                break;
+            default:
+                //Not supposed to be here
+                break;
+        }
+
+        UnityEngine.Debug.Log(current_state);
     }
 
     public override void Handle_Audio()
     {
         //Handle Footsteps
-        if (current_state == 2 && Time.time > next_step_time)
+        if ((current_state == 2 || current_state == 3) && Time.time > next_step_time)
         {
             //Play footsteps
             PlayFootstep();
             next_step_time = Time.time + move_sound_interval;
         }
-        else if (current_state != 2)
+        else if (current_state != 2 && current_state != 3)
         {
             //Stop playing
             move_source.Stop();
@@ -176,11 +269,74 @@ public class Librarian : Enemy
             if (UnityEngine.Random.value <= chatter_playchance)
             {
                 //Random Chatter Played
-                UnityEngine.Debug.Log("Chatter Played");
                 PlayRandomChatter();
             }
             // Set the time for the next possible chatter sound
             chatter_nextchattertime = Time.time + UnityEngine.Random.Range(chatter_mininterval, chatter_maxinterval);
+        }
+    }
+
+    void DetectPlayer()
+    {
+        // Line of Sight Detection (Raycast)
+        RaycastHit hit;
+        Vector3 forward = transform.forward;
+
+        if (Physics.Raycast(transform.position, forward, out hit, sightRange, playerLayer))
+        {
+            if (hit.collider != null)
+            {
+                UnityEngine.Debug.Log(hit.collider);
+                current_state = 3;
+            }
+        }
+
+        //Distance collision
+        //Set to player location
+        Player player = level.player;
+        float distanceToPlayer = Vector3.Distance(transform.position, player.gameObject.transform.position);
+
+        if (distanceToPlayer <= 5.0f)
+        {
+            current_state = 3;
+            last_known_location = player.gameObject.transform.position;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward * sightRange); // For sight detection
+    }
+
+    void DetectHidingPlayer()
+    {
+        //Set to player location
+        Player player = level.player;
+
+        // Check if the player is still in sight
+        RaycastHit hit;
+        Vector3 directionToPlayer = (player.gameObject.transform.position - transform.position).normalized;
+
+        if (Physics.Raycast(transform.position, directionToPlayer, out hit, sightRange, playerLayer))
+        {
+            if (hit.collider != null)
+            {
+                UnityEngine.Debug.Log(hit.collider);
+                last_known_location = player.gameObject.transform.position;
+            }
+            else
+            {
+                current_state = 2;
+                searching = false;
+                search_timer = search_duration;
+            }
+        }
+        else
+        {
+            current_state = 2;
+            searching = false;
+            search_timer = search_duration;
         }
     }
 
@@ -243,7 +399,7 @@ public class Librarian : Enemy
         if (isattacking == true)
         {
             //Set state to 3
-            current_state = 3;
+            current_state = 4;
             can_attack = false;
         }
     }
@@ -271,5 +427,24 @@ public class Librarian : Enemy
         direction.y = 0;
         Quaternion targetRotation = Quaternion.LookRotation(direction);
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 1.00f);
+    }
+
+    void SetNextWaypoint()
+    {
+        if (waypoints.Length == 0)
+        {
+            return;
+        }
+
+        //Randomly select new waypoint
+        int new_index;
+        do
+        {
+            new_index = UnityEngine.Random.Range(0, waypoints.Length);
+        }
+        while (new_index == current_index);
+
+        current_index = new_index;
+        current_waypoint = waypoints[current_index];
     }
 }
